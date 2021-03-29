8b608bdb321215c02836f47b3e0feaebf20c2c8e
Barnaby Napier <barnabynapier@gmail.com>
2021-03-29 21:10:36 +0100

    Remove config and edit constants

--- config/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  db:
-    "mongodb+srv://barney:napierb@cluster1-ugvrc.mongodb.net/lectio1?retryWrites=true&w=majority",
-  db_dev:
-    "mongodb+srv://barney:napierb@cluster1-ugvrc.mongodb.net/lectio1?retryWrites=true&w=majority",
-};
