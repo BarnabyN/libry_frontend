8e22b88f4de7b451f45b15056b60958cc33586a3
Barnaby Napier <barnabynapier@gmail.com>
2021-03-30 21:00:25 +0100

    Add conditional rendering to navbar

--- src/components/CustomNavbar.js
+++ src/components/CustomNavbar.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import * as constants from "../constants";
 import {
   Collapse,
   Navbar,
@@ -7,10 +8,25 @@ import {
   Nav,
   NavItem,
   NavLink,
+  NavbarText,
 } from "reactstrap";
 
 export default function CustomNavbar() {
   const [collapsed, setCollapsed] = useState(true);
+  const [user, setUser] = React.useState("");
+  const token = localStorage.getItem("token");
+
+  React.useEffect(() => {
+    if (token) {
+      fetch(constants.dbstring + "/get-current-user", {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+        .then((res) => res.json())
+        .then((u) => {
+          setUser(u["username"]);
+        });
+    }
+  }, [token]);
 
   const toggleNavbar = () => setCollapsed(!collapsed);
 
@@ -20,15 +36,22 @@ export default function CustomNavbar() {
         <NavbarBrand href="/" className="mr-auto">
           libry
         </NavbarBrand>
+        <NavbarText>{user}</NavbarText>
         <NavbarToggler onClick={toggleNavbar} className="mr-2" />
         <Collapse isOpen={!collapsed} navbar>
           <Nav navbar>
-            <NavItem>
-              <NavLink href="/login/">Log In</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="/signup">Sign Up</NavLink>
-            </NavItem>
+            {user ? (
+              <NavLink href="/login/">Log Out</NavLink>
+            ) : (
+              <div>
+                <NavItem>
+                  <NavLink href="/login/">Log In</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="/signup">Sign Up</NavLink>
+                </NavItem>
+              </div>
+            )}
           </Nav>
         </Collapse>
       </Navbar>
